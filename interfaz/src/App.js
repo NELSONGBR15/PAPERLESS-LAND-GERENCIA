4379178d48eeec6487e177b27ae3fd5f97613b3d
NelsonFBR15 <bkanegrarivera@gmail.com>
2022-04-20 20:35:52 -0500

    Interfaz Login

--- interfaz/src/App.js
+++ interfaz/src/App.js
@@ -1,11 +1,13 @@
 
 
 import Header from "./Components/Header";
+import Login from './Pages/LoginPage';
 
 function App() {
   return (
     <div >
     <Header />
+    <Login />
 
     </div>
   );
