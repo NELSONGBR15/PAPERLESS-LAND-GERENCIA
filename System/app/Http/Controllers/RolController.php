d74f3ce7b68d8774ee7eafd0fa95ea18b7d8d3f2
JonSopMac <jcabreraca@unadvirtual.edu.co>
2022-04-26 21:37:02 -0500

    Creación y configuración de tabla Rol

---
+++ System/app/Http/Controllers/RolController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Rol;
+
+class RolController extends Controller
+{
+    public function index()
+    {
+        $rol = Rol::all();
+        return $rol;
+    }
+    
+    public function create()
+    {
+        //
+    }
+    
+    public function store(Request $request)
+    {
+        $rol = new Rol();
+        $rol->nombre = $request->nombre;
+
+        $rol->save();
+    }
+
+    public function show($id)
+    {
+        $rol = Rol::find($id);
+        return $rol;
+    }
+
+    public function edit($id)
+    {
+       //
+    }
+
+    public function update(Request $request, Rol $genero)
+    {
+        $rol = Rol::findOrfail($request->id);
+        $rol->nombre = $request->nombre;
+        $rol->save();
+        return $rol;
+    }
+    public function destroy($id)
+    {
+        $rol = Rol::destroy($id);
+        return $rol;
+    }
+}
--- System/routes/api.php
+++ System/routes/api.php
@@ -2,6 +2,7 @@
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\GeneroController;
 use App\Http\Controllers\CargoController;
+use App\Http\Controllers\RolController;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -28,5 +29,14 @@ Route:: controller(CargoController::class)->group(function () {
    Route::get('/show-cargo/{id}','show');
    Route::put('/update-cargo/{id}','update');
    Route::delete('/destroy-cargo/{id}','destroy');
+
+});
+
+Route:: controller(RolController::class)->group(function () {
+   Route::get('/index-rol','index');
+   Route::post('/store-rol','store');
+   Route::get('/show-rol/{id}','show');
+   Route::put('/update-rol/{id}','update');
+   Route::delete('/destroy-rol/{id}','destroy');
 
 });
